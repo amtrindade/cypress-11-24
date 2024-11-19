6fd64c3150268e45aa4ce1d44eec0080ec8d9f63
Antonio Trindade <atrindade@umov.me>
2024-11-19 19:58:41 -0300

    Add describe-it

---
+++ cypress/e2e/describe-it-spec.cy.js
@@ -0,0 +1,35 @@
+
+describe('Group of tests B', () => {
+
+    it('Teste 5', () => {
+        console.log('Teste 5')
+    })
+
+    it('Teste 6', () => {
+        console.log('Teste 6')
+    })
+
+    describe('Group of tests C', () => {
+
+        it('Teste 7', () => {
+            console.log('Teste 7')
+        })
+    })
+
+})
+
+describe('Group of tests A', () => {
+
+    it('Teste 1', () => {
+        console.log('Teste 1')
+    })
+
+    it('Teste 2', () => {
+        console.log('Teste 2')
+    })
+
+    it('Teste 3', () => {
+        console.log('Teste 3')
+    })
+
+})
