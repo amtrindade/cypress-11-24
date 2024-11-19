9093370a8ad981ca129eab8888396ec1a79d0ba9
Antonio Trindade <atrindade@umov.me>
2024-11-19 19:41:31 -0300

    Inserido validacao

--- cypress/e2e/first-spec.cy.js
+++ cypress/e2e/first-spec.cy.js
@@ -1,9 +1,15 @@
 
 describe('WebElements Tests', () => {
 
-  it('Deveria escrever Hello World', () => {
+  it('Deveria validar textField com texto Hello World ', () => {
+
     cy.visit('https://antoniotrindade.com.br/treinoautomacao/elementsweb.html')
-    cy.get('input[name=txtbox1]').type('Hello World')
+
+    //1 - Identifica um elemento
+    //2 - Interage com esse elemento
+    //3 - Valida o resultado
+    cy.get('input[name=txtbox1]').type("Hello World!").should('have.value', 'Hello World!')
+
   })
 
 })
