4dc3e585d70a8a0524916083ee14a485f6f90270
Antonio Trindade <atrindade@umov.me>
2024-11-19 21:13:53 -0300

    add types

--- cypress/e2e/assert-spec.cy.js
+++ cypress/e2e/assert-spec.cy.js
@@ -27,4 +27,41 @@ describe('Trabalhando com asserts', () => {
     expect(str1).to.match(/^\D{31}$/)
 
   })
+
+  it('Work with boolean', () => { 
+    
+    const bool1 = true
+    expect(bool1).to.be.true
+    expect(bool1).to.be.not.false
+
+  })
+
+  it('Work with Float', () => {
+    const float1 = 10.5787
+
+    expect(float1).to.be.equal(10.5787)
+    expect(float1).to.be.closeTo(10.5, 0.1)
+
+  })
+
+  it('Work with Array', () => {
+
+    const arr1 = ['Cypress', 'Automação', 'Testes']
+
+    expect(arr1).to.be.deep.equal(['Cypress', 'Automação', 'Testes'])
+    expect(arr1).to.be.have.length(3)
+    expect(arr1).to.include.members(['Automação', 'Testes'])
+
+  })
+
+  it('Work with Object', () => {
+
+    const obj1 = [1, 2, 3]
+    const obj2 = [1, 2, 3]
+    //Compara dois objetos
+    expect(obj1).to.be.deep.equal(obj2)
+
+  })
+
+
 })
