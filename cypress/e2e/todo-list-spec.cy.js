46cc588c58e1af92fd6d8d8654eaef5e06a19730
Antonio Trindade <atrindade@umov.me>
2024-11-25 20:31:03 -0300

    Criados todo list test

--- cypress/e2e/todo-list-spec.cy.js
+++ cypress/e2e/todo-list-spec.cy.js
@@ -1,26 +1,62 @@
 
 describe('Teste de todo list', () => {
 
+    const task = 'Wash the dishes'
+
     beforeEach(() => { 
         cy.visit('https://example.cypress.io/todo#/')
     })
 
     it('Adicionar tarefa', () => {
-        cy.get('[data-test="new-todo"]').type('Lavar a louça{enter}')
+        cy.get('[data-test="new-todo"]').type(`${task}{enter}`)
 
-        cy.get('.todo-list').contains('Lavar a louça').should('be.visible')
+        cy.get('.todo-list').contains(`${task}`).should('be.visible')
 
         cy.get('.todo-list li').should('have.length', 3)
     })
 
-    it.only('Marcar tarefa como concluida', () => {
+    describe('Testes que consideram o setup tarefa adicionada', () => {
+    
+        beforeEach(() => {
+            cy.get('[data-test="new-todo"]').type(`${task}{enter}`)
+        })
 
-        cy.get('[data-test="new-todo"]').type('Lavar a louça{enter}')
-        cy.get('.todo-list li').contains('Lavar a louça').parent().find('input').check()
-        cy.get('.filters').contains('Completed').click()
-        
-        cy.get('.todo-list').contains('Lavar a louça').should('be.visible')
-        cy.get('.todo-list li').should('have.length', 1)
+        it('Marcar tarefa como concluida', () => {
+           
+            cy.get('.todo-list li').contains(`${task}`).parent().find('input').check()
+            cy.get('.filters').contains('Completed').click()
+            
+            cy.get('.todo-list').contains(`${task}`).should('be.visible')
+            cy.get('.todo-list li').should('have.length', 1)
+        })
+
+        it('Remover nova tarefa adcionada', () => {
+            cy.get('.todo-list li').contains(`${task}`).parent().find('input').check()
+
+            // 1 - Usando o trigger para simular o click do mouse
+            // cy.get('.todo-list li')
+            //     .contains(`${task}`)
+            //     .parent().find('button')
+            //     .trigger('mouseover').click()
+
+            //2 - Usando o invoke para simular o click do mouse
+            cy.get('.todo-list li')
+                .contains(`${task}`)
+                .parent().find('button')
+                .invoke('show').click()
+            
+            // 3 - Usando o force para simular o click do mouse
+            // cy.get('.todo-list li')
+            //     .contains(`${task}`)
+            //     .parent().find('button')
+            //     .click( {force: true} )
+            
+            cy.get('.todo-list li')
+                .should('have.length', 2)
+                .should('not.contain', `${task}`)
+            
+        })
+
     })
 
 })
