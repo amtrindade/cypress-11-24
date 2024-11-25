24ddcc120dc072baf67a40a7b42ac735ba4da286
Antonio Trindade <atrindade@umov.me>
2024-11-25 19:54:51 -0300

    Task done

--- cypress/e2e/todo-list-spec.cy.js
+++ cypress/e2e/todo-list-spec.cy.js
@@ -1,7 +1,7 @@
 
 describe('Teste de todo list', () => {
 
-    before(() => { 
+    beforeEach(() => { 
         cy.visit('https://example.cypress.io/todo#/')
     })
 
@@ -11,6 +11,16 @@ describe('Teste de todo list', () => {
         cy.get('.todo-list').contains('Lavar a louça').should('be.visible')
 
         cy.get('.todo-list li').should('have.length', 3)
+    })
+
+    it.only('Marcar tarefa como concluida', () => {
+
+        cy.get('[data-test="new-todo"]').type('Lavar a louça{enter}')
+        cy.get('.todo-list li').contains('Lavar a louça').parent().find('input').check()
+        cy.get('.filters').contains('Completed').click()
+        
+        cy.get('.todo-list').contains('Lavar a louça').should('be.visible')
+        cy.get('.todo-list li').should('have.length', 1)
     })
 
 })
